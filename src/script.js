0eda26c8f9940a0d3cd2b1736f3f729c28a67797
scherepi <scherejoaquin@gmail.com>
2026-04-13 03:16:55 -0500

    set up blinking eye grid!

--- src/script.js
+++ src/script.js
@@ -10,9 +10,34 @@ function animateTitle() {
     wingRight.classList.add("transition-init");
 }
 
+function fillEyes() {
+    let eyeGrid = document.getElementById("eyes-grid");
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 8; j++) {
+            let eyeContainer = document.createElement("div")
+            eyeContainer.id = `eye-${i}${j}`
+            eyeContainer.classList.add("eye-container");
+            eyeContainer.style.backgroundImage = "url(../data/eyeopen.gif)";
+            eyeContainer.style.gridArea = `a${i}${j}`
+            setTimeout(() => { eyeContainer.style.backgroundImage = "url(../data/eyefull.png)"; }, 1000)
+            eyeGrid.appendChild(eyeContainer);
+        }
+    }
+}
+
+function blinkRandom() {
+    setInterval(() => {
+        let targetEye = document.getElementById(`eye-${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 8)}`);
+        targetEye.style.backgroundImage = "url(../data/blink.gif)";
+        setTimeout(() => {targetEye.style.backgroundImage = "url(../data/eyefull.png)"}, 2000)
+    },
+    Math.random() * 6000)
+}
+
 document.body.onload = () => {
     // trigger the initial transition by appending a class to the relevant elements
     console.log("doc and script loaded");
     animateTitle();
-    fillEyes()
+    fillEyes();
+    blinkRandom();
 }
